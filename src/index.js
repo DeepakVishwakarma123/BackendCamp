0346fd9c09fa0e5fcd6fdd202e789591a4b7ddb6
DeepakLinux <deepakvishwakarmajs@gmail.com>
2026-01-04 00:59:33 +0530

    file modified making app seperation to app.js make things modular

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
 import dotenv from "dotenv"
-import express from "express"
+import app from "./app.js"
 
 dotenv.config({path:"./.env"})
 
-let app=express()
+
 const port=process.env.PORT
+console.log(process.env.originslist);
 
-
-app.get(
-    "/",(req,res) => {
-           res.send("hello you are hitting /route")
-    }
-)
-
-app.get(
-    "/ideas",(req,res) => {
-        
-       let text="hello user you currently hitting /ideas route"
-       res.send(`response received ${text}`)
-    }
-)
-app.get("/username", (req,res) => {
-    res.send("hello user from hitting /username routes")
-})
-
-app.get("/domain",(req,res) => {
-    res.send("404,not found page")
-})
 
 app.listen(port,() => {console.log(`listening on http://localhost:${port}`)})
 
