0aacba0bd25eea789fbb6f077092783e2a6353bf
DeepakLinux <deepakvishwakarmajs@gmail.com>
2026-01-04 15:09:17 +0530

    logger code add to just track whether request are coming or not

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import events from "node:events"
+import fs from "node:fs"
+import { log } from "node:console"
+
+const EventEmitter=events
+
+class logger extends  EventEmitter
+{
+    log(message)
+    {
+        this.emit("message",message)
+    }
+}
+
+let loggerObject=new  logger()
+
+
 
 dotenv.config({path:"./.env"})
 
@@ -20,9 +37,10 @@ app.use(express.urlencoded(
 app.use(cors(
     {
         // origin:process.env.originslist.split(",")
-        origin:["http://localhost/5173","https://hoppscotch.io"],
+        origin:["http://localhost/5173"],
         methods:["get","post","put","delete","options"],
-        credentials:true
+        credentials:true,
+        maxAge:60
     }
 ))
 
@@ -35,25 +53,51 @@ app.use(express.static("./Assests"))
 
 app.get(
     "/",(req,res) => {
-           res.send("hello you are hitting /route")
+        res.send("hello you are hitting cheating /routes")
+        let message=generateMessageLog(req)
+        loggerObject.log(message)        
     }
 )
 
 app.get(
-    "/ideas",(req,res) => {
-        
+    "/ideas",(req,res) => {  
        let text="hello user you currently hitting /ideas route"
-       res.send(`response received ${text}`)
+       res.send(`response receivedss ${text}`)
+       let message=generateMessageLog(req)     
+       loggerObject.log(message)        
     }
 )
 app.get("/username", (req,res) => {
-    res.send("hello user from hitting /username routes")
+    res.send("hello user from hitting /username routes done")
+    let message=generateMessageLog(req)
+    loggerObject.log(message)        
 })
 
 app.get("/domain",(req,res) => {
-    res.send("404,not found page")
+    res.send("404,not found pagess")
+    let message=generateMessageLog(req)
+    loggerObject.log(message)
 })
 
+
+loggerObject.on(
+    //loggeobject function log message value is get here as that event occurss
+    "message",(messageValue) => {
+        let filepath="./log.txt"
+        console.log("message value passed is hrere",messageValue);
+        fs.appendFileSync(filepath,messageValue)
+        
+    }
+)
+
+function generateMessageLog(req)
+{
+    let url=req.url
+    let Dateobject=new Date()
+    let timewithDate=Dateobject.toLocaleTimeString()
+    let messageLog=`Url is:- ${url} DateTime:- ${timewithDate} \n`
+    return messageLog
+}
 export default app
 
 
