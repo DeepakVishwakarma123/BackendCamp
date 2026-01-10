109c49767135f426aafe5529ecfd97867ce32f45
DeepakLinux <deepakvishwakarmajs@gmail.com>
2026-01-10 09:15:49 +0530

    utility add apiresponse,apierror,constants,asyn handler

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+//constants holds this file 
+//in every web application thee are some fields whihc are acts as a fixed value you can say
+//that can not changed through out the project and literally they are useable every time
