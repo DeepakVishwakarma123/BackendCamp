a57a385154c059a4a348fdf06eedc9cf1f986e7c
DeepakLinux <deepakvishwakarmajs@gmail.com>
2026-02-05 23:14:54 +0530

    register route middlware attached

--- src/routes/register-route.js
+++ src/routes/register-route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import validate from "../validators/validate.js";
+import validateMiddleWare from "../middleware/validate-result-middleware.js";
 import registerController from "../controllers/Register-controller.js";
 
 const RegisterRouter=Router()
 
-RegisterRouter.route("/").post(registerController)
+RegisterRouter.route("/").post(validate(),validateMiddleWare,registerController)
 
 export default RegisterRouter
