3b2ba7861e65f2fcece8208c8a279ca086697a9e
DeepakLinux <deepakvishwakarmajs@gmail.com>
2026-02-12 15:46:39 +0530

    get current user ,forgot password controller add

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -2,6 +2,8 @@ import asyncHandler from "../utils/async-handlers.js";
 import apiError from "../utils/api-error.js";
 import apiResponse from "../utils/api-response.js";
 import User from "../models/user-model.js";
+import emailContentBodyGenWithUrl from "../utils/emailContentGenertionFunction.js";
+import sendEmail from "../utils/sendEmailUtility.js";
 
 let loginController=asyncHandler(
 async function (req,res,next)
@@ -63,4 +65,46 @@ let logoutController=asyncHandler(
     }
 )
 
-export {loginController,logoutController}
+let getCurrentUser=asyncHandler(
+    async function (req,res) {
+        let userDoc=req.user
+        let user=await User.findById(userDoc._id).select("-password -refreshToken -forgotPasswordToken -forgotPasswordExpiry -emailVerficationToken -emailVerificationExpiry")
+        res.status(200).json(
+            new apiResponse(200,"current user info",user)
+        )
+    }
+)
+
+let forgotPasswordRequest=asyncHandler(
+    async function () {
+        //getting email from user input
+        let {email}=req.body
+        let UserDoc=await User.findOne({email:email})
+        if(userDoc)
+        {
+            let {tokenWithoutHash,tokenExpiry,hashedToken}=UserDoc.GenerateTokenWithoutData()
+            UserDoc.forgotPasswordToken=hashedToken
+            UserDoc.forgotPasswordExpiry=tokenExpiry
+            let emailUrl=`${req.protocol}://${req.get("host")}/api/v1/auth/reset-password/${tokenWithoutHash}`
+            //email working
+            let emailGenContent=emailContentBodyGenWithUrl(
+                {
+                    username:UserDoc.username,
+                    intro:"password reset email",
+                    instructions:"click on below link to start the process",
+                    linkText:"reset password",
+                    outro:"system generated email do not reply to it"
+                },
+                emailUrl
+            )
+            let mailRes=await sendEmail({mailContent:emailGenContent,receiverEmailAddress:email})
+            res.status(200).json(
+                new apiResponse(200,"forgot password email is sended",{
+                mailsend:"true"
+                })
+            )
+        }
+    }
+)
+
+export {loginController,logoutController,getCurrentUser}
