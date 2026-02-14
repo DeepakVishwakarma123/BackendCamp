e8761c6b2b3388702c1d9f246c5105bd16805116
DeepakLinux <deepakvishwakarmajs@gmail.com>
2026-02-14 22:27:53 +0530

    routes add

--- src/routes/auth-route.js
+++ src/routes/auth-route.js
@@ -1,15 +1,26 @@
 import { Router} from "express";
 import { loginController } from "../controllers/auth-controller.js";
-import { loginValidate } from "../validators/validate.js";
+import { loginValidate, passwordChange, userForgotPasswordValidator, userResetPasswordForgotValidator } from "../validators/validate.js";
 import validateMiddleWare from "../middleware/validate-result-middleware.js";
 import { logoutController } from "../controllers/auth-controller.js";
 import authMiddlware from "../middleware/auth-middlware.js";
+import { getCurrentUser } from "../controllers/auth-controller.js";
+import { refreshAccessToken} from "../controllers/auth-controller.js";
+import { changePassword } from "../controllers/auth-controller.js";
+import { resetPassword } from "../controllers/auth-controller.js";
+import { forgotPasswordRequest } from "../controllers/auth-controller.js";
 
 const authRouter=Router()
 
 //
 authRouter.route("/login").post(loginValidate(),validateMiddleWare,loginController)
 authRouter.route("/logout").post(authMiddlware,logoutController)
+
+authRouter.route("/current-user").get(authMiddlware,getCurrentUser)
+authRouter.route("/refresh-token").post(refreshAccessToken)
+authRouter.route("/forgot-password").post(userForgotPasswordValidator(),validateMiddleWare,forgotPasswordRequest)
+authRouter.route("/reset-password/:resetToken").post(userResetPasswordForgotValidator(),validateMiddleWare,resetPassword)
+authRouter.route("/change-password").post(authMiddlware,passwordChange(),validateMiddleWare,changePassword)
 
 
 export default authRouter
